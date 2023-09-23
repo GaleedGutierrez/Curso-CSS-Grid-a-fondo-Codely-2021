18822b4cd5f93dc7a5af75f515c9e12609fbe2d2
Galeed Gutierrez <galeedeveloper@gmail.com>
2023-09-23 03:31:18 -0300

    :lipstick: Clase: Hacemos nuestro grid responsive

---
+++ build/main.244bab53c51e5bbfbfd8.hot-update.js
@@ -0,0 +1,22 @@
+self["webpackHotUpdatecurso_css_grid_a_fondo_codely_2021"]("main",{
+
+/***/ "./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./scss/index.scss":
+/*!******************************************************************************************************!*\
+  !*** ./node_modules/css-loader/dist/cjs.js!./node_modules/sass-loader/dist/cjs.js!./scss/index.scss ***!
+  \******************************************************************************************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nExpected expression.\n    ╷\n160 │         padding-block-start:;\n    │                             ^\n    ╵\n  scss\\index.scss 160:23  root stylesheet");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("aea40ea4a5f4e48f9ee0")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=main.244bab53c51e5bbfbfd8.hot-update.js.map
